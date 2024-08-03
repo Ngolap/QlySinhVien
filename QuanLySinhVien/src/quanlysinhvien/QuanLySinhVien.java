22aa51a581d2df6c5d79e94a4bc225175c0a9b0d
Lập caption <160254060+Ngolap@users.noreply.github.com>
2024-08-03 23:17:39 +0700

    Phần mền quản lý sinh viên

---
+++ QuanLySinhVien/src/quanlysinhvien/QuanLySinhVien.java
@@ -0,0 +1,12 @@
+package quanlysinhvien;
+
+public class QuanLySinhVien {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
---
+++ QuanLySinhVien/src/quanlysinhvien/Student.java
@@ -0,0 +1,31 @@
+package quanlysinhvien;
+
+import java.io.Serializable;// b1
+
+public class Student  implements Serializable  {
+   public String name;
+   public double marks;
+    public String major;
+    public String getGrade() {
+        if (this.marks < 3) {
+            return "Kém";
+        }
+        if (this.marks < 5) {
+            return "Yếu";
+        }
+        if (this.marks < 6.5) {
+            return "Trung bình";
+        }
+        if (this.marks < 7.5) {
+            return "Khá";
+        }
+        if (this.marks < 9) {
+            return "Giỏi";
+        }
+        return "Xuất sắc";
+    }
+
+    public boolean isBonus() {
+        return this.marks >= 7.5;
+    }
+}
